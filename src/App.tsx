21c5405546ab2588ef35d8a331b73bfb89ee03b4
Kevin Matson <matson.kevin@yahoo.com>
2022-09-17 13:09:08 -0500

    First quote

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const quotes: [string, string][] = [
+  ['I do not eat breakfast because breakfast is for nerds.', 'September 10th, 2022'],
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Quote</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {quotes.map(([quote, date]) => (<tr>
+          <td>{quote}</td>
+          <td>{date}</td>
+        </tr>))}
+      </tbody>
+    </table>
   );
 }
 
