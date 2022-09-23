a9c801a1bab6799275c59c191f07ef97bcfd5eec
Kevin Matson <matson.kevin@yahoo.com>
2022-09-23 09:47:12 -0500

    Added another quote

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 const quotes: [string, string][] = [
+  ["What's the worst that could happen? He'll be a virgin twice?", 'like the second session or something'],
   ['I do not eat breakfast because breakfast is for nerds.', 'September 10th, 2022'],
 ];
 
