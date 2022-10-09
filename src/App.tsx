d565e5fa1b198ed4e9eb858759eb901e5d307ade
Kevin Matson <kevin.g.matson@gmail.com>
2022-10-08 22:39:24 -0500

    New quote, updated dates

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,11 @@ const quotes: [string|React.ReactElement, string][] = [
     <span style={{ fontSize: '100%'}}>
       I cannot wait to use my imposing figure to intimidate kobolds. Using my imposing figure to intimidate <span style={{fontSize: '80%'}}> kobolds is the best... thing and most amount of fun you can have while being in a cave. I can see why all <span style={{fontSize: '80%'}}>the kids are hanging out in caves these days. It's because they like</span> to intimidate kobolds with their imposing figure.</span> I <em>also</em> like to intimidate kobolds with my imposing figure. It is the most fun you can possibly have.
     </span>,
-    'like the fourth session or something'
+    'Session 5'
   ],
-  ['Th-th-th-tha-th-th-than-than-thank y-y-y-you, y-y-y-your M-m-m-m-m-maje-je-je-jesty.', 'idk, session 7?'],
+  ['Th-th-th-tha-th-th-than-than-thank y-y-y-you, y-y-y-your M-m-m-m-m-maje-je-je-jesty.', 'Session 8'],
   ['I do not eat breakfast because breakfast is for nerds.', 'September 10th, 2022'],
+  ["It's Bodhin' time", 'October 8th, 2022']
 ];
 
 function App() {
